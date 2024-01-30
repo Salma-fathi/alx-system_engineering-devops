8eaddad9bd7b648ce952dfdddce8c80bd396371c
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-01-31 02:09:05 +0300

    fffollg

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,21 +1,9 @@
 #!/usr/bin/env ruby
 
-# Accept the log file as an argument
-log_file = ARGV[0]
+Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
+List = [Format[0].compact, Format[1].compact, Format[2].compact]
+puts List.join(',')
 
-# Read the log file line by line
-File.open(log_file).each do |line|
-  # Extract relevant information using regular expressions
-  match_data = line.match(/\[from:([^\]]+)\] \[to:([^\]]+)\] \[flags:([^\]]+)\]/)
-
-  # Extract sender, receiver, and flags
-  sender = match_data[1]
-  receiver = match_data[2]
-  flags = match_data[3]
-
-  # Output the extracted information
-  puts "#{sender},#{receiver},#{flags}"
-end
 
 
 
