3c373ad3ab0d21705bded653fc301e77627347c1
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-01-31 02:07:46 +0300

    fxhhx

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,15 +1,21 @@
 #!/usr/bin/env ruby
-# Accept the argument passed to the script# Define the regular expression pattern to match a sequence of 'hbt' followed by 1 to 4 't' characters and ending with 'n'
-# Match the pattern against the argument
-# Print the matched string(s)
 
-arg = ARGV[0]
-pattern = /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/
-match = arg.scan(pattern)
-puts match.join
+# Accept the log file as an argument
+log_file = ARGV[0]
 
+# Read the log file line by line
+File.open(log_file).each do |line|
+  # Extract relevant information using regular expressions
+  match_data = line.match(/\[from:([^\]]+)\] \[to:([^\]]+)\] \[flags:([^\]]+)\]/)
 
+  # Extract sender, receiver, and flags
+  sender = match_data[1]
+  receiver = match_data[2]
+  flags = match_data[3]
 
+  # Output the extracted information
+  puts "#{sender},#{receiver},#{flags}"
+end
 
 
 
