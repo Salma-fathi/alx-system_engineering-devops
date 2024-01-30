0b22f6772fd8c312416027f4489b863b17ab7cdf
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-01-31 02:04:00 +0300

    fainaalla

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# Accept the argument passed to the script# Define the regular expression pattern to match a sequence of 'hbt' followed by 1 to 4 't' characters and ending with 'n'
+# Match the pattern against the argument
+# Print the matched string(s)
+
+arg = ARGV[0]
+pattern = /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/
+match = arg.scan(pattern)
+puts match.join
+
+
+
+
+
+
+
+
+
+
+
+
