63b3577695e073e5c9d3917d8749977290a6ca52
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-01-31 01:46:42 +0300

    vsfdbdbe

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# Accept the argument passed to the script# Define the regular expression pattern to match a sequence of 'hbt' followed by 1 to 4 't' characters and ending with 'n'
+# Match the pattern against the argument
+# Print the matched string(s)
+
+arg = ARGV[0]
+pattern = /hbt*n/
+match = arg.scan(pattern)
+puts match.join
