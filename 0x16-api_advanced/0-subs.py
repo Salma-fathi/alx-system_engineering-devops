315b4af22dee3c09cbf631387f0431aadaf3ed0f
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-08-13 18:23:58 +0200

    taks0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    try:
+        data = response.json()
+        return data['data']['subscribers']
+    except:
+        return 0
