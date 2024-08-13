60ef39cf46f2128f395bcf98fb3e2f2aed9ef835
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-08-13 19:18:23 +0200

    re

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
+"""Contains top_ten function"""
+import requests
 
-import praw
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API for the top 10 hot posts of a given subreddit
-    and prints their titles.
-
-    Args:
-        subreddit (str): The name of the subreddit to search.
-
-    Returns:
-        None if the subreddit is invalid.
-    """
-    reddit = praw.Reddit(client_id="[YOUR_CLIENT_ID]",
-                        client_secret="[YOUR_CLIENT_SECRET]",
-                        user_agent="[YOUR_USER_AGENT]")  # Replace with your Reddit app credentials
-
-    try:
-        subreddit = reddit.subreddit(subreddit)
-        for post in subreddit.hot(limit=10):
-            print(post.title)
-    except praw.exceptions.HTTPException as error:
-        # Handle potential errors like invalid subreddit or API issues
-        print(f"Error: {error}")
-        return None
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
